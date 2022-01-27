e45416ccf1803fdc33c842c49b387362bd35e6ee
KDSigel <heykarl@gmail.com>
2022-01-27 12:04:30 -0800

    added second table for art

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS bikes;
+DROP TABLE IF EXISTS bikes, art;
 
 CREATE TABLE bikes (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -12,3 +12,14 @@ CREATE TABLE bikes (
 
 INSERT INTO bikes (model, ride, love)
 VALUES ('Stumpjumper', 'false', '10');
+
+CREATE TABLE art (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    theme VARCHAR NOT NULL,
+    medium VARCHAR NOT NULL,
+    worth money
+);
+
+INSERT INTO art (title, theme, medium, worth)
+VALUES ('drunkman', 'superhero, booze', 'oil paint', '$200.00');
