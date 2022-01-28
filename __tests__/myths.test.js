fc068a51ac3b589365462219c9578c88a17b82a9
KDSigel <heykarl@gmail.com>
2022-01-28 12:08:29 -0800

    added test for getting all myths and not passing

--- __tests__/myths.test.js
+++ __tests__/myths.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Myth = require('../lib/models/Myth');
 
 describe('Art backend routes', () => {
   // let testMyth;
@@ -27,4 +28,23 @@ describe('Art backend routes', () => {
       believability: 'low',
     });
   });
+
+  it('gets all myths when I do a get', async () => {
+    await Myth.insert({
+      title: 'religion',
+      pervasiveness: 'waning',
+      believability: 'low',
+    });
+    const res = await request(app).get('/api/v1/myths');
+
+    expect(res.body).toContainEqual({
+      id: expect.any(String),
+      title: 'religion',
+      pervasiveness: 'waning',
+      believability: 'low',
+    });
+  });
+
+
+  
 });
