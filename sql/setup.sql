f16b65600fc5812a2750b1824534e31c0b54c059
KDSigel <heykarl@gmail.com>
2022-01-27 18:16:27 -0800

    added last three sql tables and default values

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS bikes, art;
+DROP TABLE IF EXISTS bikes, art, spirits, myths, smells;
 
 CREATE TABLE bikes (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -23,3 +23,34 @@ CREATE TABLE art (
 
 INSERT INTO art (title, theme, medium, worth)
 VALUES ('drunkman', 'superhero, booze', 'oil paint', '$300.00');
+
+CREATE TABLE spirits (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    category VARCHAR NOT NULL,
+    brand VARCHAR NOT NULL,
+    stocked BOOLEAN NOT NULL
+);
+
+INSERT INTO spirits (category, brand, stocked)
+VALUES ('Scotch', 'Laphroaig', 'true');
+
+CREATE TABLE myths (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    pervasiveness VARCHAR NOT NULL,
+    believability VARCHAR NOT NULL
+);
+
+INSERT INTO myths (title, pervasiveness, believability)
+VALUES ('Trickel-down economics', 'medium', 'high');
+
+CREATE TABLE smells (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    strength VARCHAR NOT NULL,
+    enjoyable BOOLEAN NOT NULL
+);
+
+INSERT INTO smells (title, strength, enjoyable)
+VALUES ('garlic', 'medium', 'true');
+
