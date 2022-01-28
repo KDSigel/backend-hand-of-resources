9decd0cb3779d3ac91200e2f71e33c6129485e63
KDSigel <heykarl@gmail.com>
2022-01-28 09:36:12 -0800

    added test for get by ID that is failing

--- __tests__/spirits.test.js
+++ __tests__/spirits.test.js
@@ -2,11 +2,12 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Spirit = require('../lib/models/Spirit');
 
 describe('Spirits backend routes', () => {
-  //   let testSpirit;
+    let testSpirit;
   beforeEach(async () => {
-    // [testSpirit] = await Spirit.getAll();
+    [testSpirit] = await Spirit.getAll();
     return setup(pool);
   });
 
@@ -27,4 +28,31 @@ describe('Spirits backend routes', () => {
       stocked: true,
     });
   });
+
+  it('gets all spirits when I do a get', async () => {
+    await Spirit.insert({
+      category: 'mezcal',
+      brand: 'Vida',
+      stocked: true,
+    });
+    const res = await request(app).get('/api/v1/spirits');
+
+    expect(res.body).toContainEqual({
+      id: expect.any(String),
+      category: 'mezcal',
+      brand: 'Vida',
+      stocked: true,
+    });
+  });
+
+  it('gets one spirit when I do a get call with ID', async () => {
+    const res = await request(app).get(`/api/v1/spirit/${testSpirit.id}`);
+    expect(res.body).toEqual({
+      id: '1',
+      category: 'Scotch',
+      brand: 'Laphroaig',
+      stocked: true,
+    });
+  });
+
 });
